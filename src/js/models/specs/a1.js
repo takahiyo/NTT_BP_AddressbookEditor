9f8fb2035ec8ffdb3ad41e80ebc2f4eedc9d1d8f
Taka <taka.hiyo@gmail.com>
2026-03-16 14:47:54 +0900

    修正10

--- src/js/models/specs/a1.js
+++ src/js/models/specs/a1.js
@@ -5,13 +5,13 @@ import { defineSpec } from '../base-fields.js';
  */
 
 const items = [
-  { key: 'ten', label: 'TEN', type: 'number', width: 'col-ten' },
+  { key: 'ten', label: 'TEN', type: 'number', width: 'col-ten', defaultValue: '0' },
   { key: 'dataType', label: 'データ種別', type: 'number', width: 'col-default', defaultValue: '1' },
   { key: 'version', label: 'バージョン', type: 'number', width: 'col-default', defaultValue: '1' },
   { key: 'memoryNo', label: 'メモリ番号', type: 'memory', width: 'col-memory' },
   { key: 'sharedGroupNo', label: '共用G', type: 'number', width: 'col-group', defaultValue: '1' },
-  { key: 'groupNo', label: 'グループ', type: 'number', width: 'col-group', defaultValue: '1' },
-  { key: 'lenType', label: 'LEN種別', type: 'number', width: 'col-default' },
+  { key: 'groupNo', label: 'グループ', type: 'number', width: 'col-group', defaultValue: '0' },
+  { key: 'lenType', label: 'LEN種別', type: 'number', width: 'col-default', defaultValue: '0' },
   { key: 'name', label: '名称', type: 'text', width: 'col-name', constraints: { maxBytes: 40, unit: 'byte', onOverflow: 'autoCut' } },
   { key: 'furigana', label: 'ﾌﾘｶﾞﾅ', type: 'text', width: 'col-furigana', constraints: { maxBytes: 12, unit: 'byte', charType: 'halfKana', onOverflow: 'error', onInvalidChar: 'error' } },
   { key: 'phone1', label: '電話番号 1', type: 'phone', width: 'col-phone', constraints: { maxBytes: 32, unit: 'byte', onOverflow: 'autoCut', onInvalidChar: 'error' } },
@@ -20,7 +20,7 @@ const items = [
   { key: 'phone2', label: '電話番号 2', type: 'phone', width: 'col-phone', constraints: { maxBytes: 32, unit: 'byte', onOverflow: 'autoCut', onInvalidChar: 'error' } },
   { key: 'icon2', label: 'アイコン 2', type: 'number', width: 'col-icon', defaultValue: '1' },
   { key: 'dialAttr2', label: '発信属性 2', type: 'number', width: 'col-dial-attr', defaultValue: '1' },
-  { key: 'dialInType', label: 'DI種別', type: 'number', width: 'col-default' },
+  { key: 'dialInType', label: 'DI種別', type: 'number', width: 'col-default', defaultValue: '0' },
   { key: 'dialInContent', label: 'DI内容', type: 'text', width: 'col-default' }
 ];
 
